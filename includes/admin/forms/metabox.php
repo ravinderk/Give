33eda92157a111ab04893365c089a46e52bfb3db
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2016-12-05 10:25:19 +0530

    Add display feature image position setting

--- includes/admin/forms/metabox.php
+++ includes/admin/forms/metabox.php
@@ -33,3 +33,57 @@ function give_add_shortcode_to_publish_metabox() {
 }
 
 add_action( 'post_submitbox_misc_actions', 'give_add_shortcode_to_publish_metabox' );
+
+
+/**
+ * Add display location setting to feature image metabox.
+ *
+ * @since 1.8
+ *
+ * @param $content
+ * @param $post_id
+ *
+ * @return string
+ */
+function give_add_featured_image_display_settings( $content, $post_id ) {
+	global $post;
+
+	// Make sure we affect only 'give_forms' post type
+	if ( 'give_forms' != $post->post_type ) {
+		return $content;
+	}
+
+	// Add 'featured_image_location' field
+	$field_id    = 'give_featured_img_pos';
+	$field_value = ( $field_value = esc_attr( get_post_meta( $post_id, $field_id, true ) ) ? $field_value : 'none' );
+	$field_text  = esc_html__( 'Image Position:', 'give' );
+
+	ob_start();
+	?>
+	<div>
+		<p>
+			<strong class="label"><?php echo $field_text; ?></strong><br>
+			<span class="give-field-description"><?php _e( 'Please select the position you would like to display for your single donation form\'s featured image.', 'give' ); ?></span>
+		</p>
+
+		<input type="radio" name="<?php echo $field_id; ?>" id="<?php echo "{$field_id}_none"?>" value="none"<?php echo checked( $field_value, 'none', false ); ?>>
+		<label for="<?php echo "{$field_id}_none"?>"><?php _e( 'None', 'give' ); ?></label><br>
+
+		<input type="radio" name="<?php echo $field_id; ?>" id="<?php echo "{$field_id}_left"?>" value="left"<?php echo checked( $field_value, 'left', false ); ?>>
+		<label for="<?php echo "{$field_id}_left"?>"><?php _e( 'Left', 'give' ); ?></label><br>
+
+		<input type="radio" name="<?php echo $field_id; ?>" id="<?php echo "{$field_id}_right"?>" value="right"<?php echo checked( $field_value, 'right', false ); ?>>
+		<label for="<?php echo "{$field_id}_right"?>"><?php _e( 'Right', 'give' ); ?></label><br>
+
+		<input type="radio" name="<?php echo $field_id; ?>" id="<?php echo "{$field_id}_above"?>" value="above"<?php echo checked( $field_value, 'above', false ); ?>>
+		<label for="<?php echo "{$field_id}_above"?>"><?php _e( 'Above The Form', 'give' ); ?></label>
+	</div>
+
+	<?php
+	$content .= ob_get_clean();
+
+	return $content;
+
+}
+
+add_filter( 'admin_post_thumbnail_html', 'give_add_featured_image_display_settings', 10, 2 );
