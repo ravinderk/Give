3858e8b7232f87f210e58049807023c2254aacaf
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2019-04-18 17:24:26 +0530

    test

--- tests/e2e/bin/install.sh
+++ tests/e2e/bin/install.sh
@@ -12,6 +12,7 @@ if [[ ${TRAVIS_PHP_VERSION:0:3} != "5.3" ]]; then
 	git clone -b ${TRAVIS_BRANCH} --single-branch https://github.com/ravinderk/give.git
 	cd ~/wordpress_data/wp-content/plugins/give/
 	docker exec give_wordpress_1 wp core update
+	cat wp-includes/version.php
 	composer install
 	docker exec give_wordpress_1 wp plugin activate give
 	rm -rf ./node_modules package-lock.json
